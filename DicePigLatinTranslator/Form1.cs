63fdec817130ced28bdf44592c4d9b7d63c972de
DiceTJ <61960729+DiceTJ@users.noreply.github.com>
2023-09-21 19:39:49 -0600

    Design Complete

--- DicePigLatinTranslator/Form1.cs
+++ DicePigLatinTranslator/Form1.cs
@@ -16,5 +16,10 @@ namespace DicePigLatinTranslator
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
