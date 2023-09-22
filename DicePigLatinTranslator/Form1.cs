c5a6210b9b2fd665c50fbabedee0c108b6258085
DiceTJ <61960729+DiceTJ@users.noreply.github.com>
2023-09-21 19:45:51 -0600

    Last Minute Changes

--- DicePigLatinTranslator/Form1.cs
+++ DicePigLatinTranslator/Form1.cs
@@ -21,5 +21,16 @@ namespace DicePigLatinTranslator
         {
 
         }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtEnglish.Text = "";
+            txtPigLatin.Text = "";
+        }
     }
 }
